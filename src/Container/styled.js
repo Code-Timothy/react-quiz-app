ef8a005433f8d1bd648eb3a4a93ebc215113edf2
Tymoteusz Wójcik <timmywojcik@gmail.com>
2023-09-10 14:59:58 +0200

    Container component added with styled-components

---
+++ src/Container/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+    width: 100%;
+    height:100vh;
+    background-color: #15cdec;
+    background-image: linear-gradient(90deg, #15cdec 0%, #56d3e6 50%, #05afee 100%);
+`;
